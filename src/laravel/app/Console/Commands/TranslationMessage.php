7b44e0f4993c6a6f9f013012e8cb4d52a653c11e
Oracle <akuznecov@scid.ru>
2023-08-04 17:53:22 +0300

    добавлен horizon, настроен redis, созданы event и консольная команда использующая его

---
+++ src/laravel/app/Console/Commands/TranslationMessage.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Events\TranslationEvent;
+use Illuminate\Console\Command;
+
+class TranslationMessage extends Command
+{
+    protected $signature = 'translation:messages {id} {message}';
+    protected $description = 'Fire event';
+
+    public function handle()
+    {
+        event(new TranslationEvent(
+            $this->argument('id'),
+            $this->argument('message')
+        ));
+    }
+}
---
+++ src/laravel/app/Events/TranslationEvent.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class TranslationEvent implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels, Queueable;
+
+    public int $id;
+    public string $message;
+
+    public function __construct($id, $message)
+    {
+        $this->id = $id;
+        $this->message = $message;
+    }
+
+    public function broadcastOn(): array
+    {
+        return [
+            new Channel('translation'. $this->id),
+        ];
+    }
+}
